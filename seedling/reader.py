cc010fab611d6f72528b699c3232e0c36590cc78
tahmidefaz <tahmidefaz@gmail.com>
2024-07-27 20:51:26 -0400

    adding YAML file validation using JSONschema

--- seedling/reader.py
+++ seedling/reader.py
@@ -1,5 +1,36 @@
 import yaml
 import glob
+import jsonschema
+
+SCHEMA = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "description": {"type": "string"},
+        "intents": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "name": {"type": "string"},
+              "description": {"type": "string"},
+            },
+            "additionalProperties": False,
+          }
+        }
+    },
+    "required": ["name", "description", "intents"],
+    "additionalProperties": False,
+}
+
+def is_valid(filename, data):
+  try:
+    jsonschema.validate(instance=data, schema=SCHEMA)
+  except jsonschema.exceptions.ValidationError as e:
+    print(f"{filename} is invalid: {e}")
+    return False
+  return True
 
 
 def read(directory):
@@ -18,7 +49,8 @@ def read(directory):
   for yaml_file in yaml_files:
     with open(yaml_file, 'r') as f:
       data = yaml.safe_load(f)
-      all_topic_info.append(data)
+      if is_valid(yaml_file, data):
+        all_topic_info.append(data)
 
   return all_topic_info
 
